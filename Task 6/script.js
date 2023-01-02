87bd6f7044626cea23358205263b1fbe6e279498
giedrius-tumosa <gie.tumosa@gmail.com>
2023-01-02 12:58:38 +0200

    Completed Task 6.

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,9 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+let getUserAverageAge = (array) => array
+  .reduce((accum, currentVal) => accum + currentVal.age, 0) / array.length;
+
+
+let getUsersNames = (array) => array.map(el => el.name);
