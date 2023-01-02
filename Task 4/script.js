39e37a73f6bb188c59c8ef3b6e774a4c2c7d6741
giedrius-tumosa <gie.tumosa@gmail.com>
2023-01-02 11:53:52 +0200

    Completed Task 4.

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,45 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+class CarBrand {
+  constructor(props) {
+    this.props = props;
+    return this.render();
+  }
+  render() {
+    const output = document.querySelector("#output");
+    const carBrandCard = document.createElement("article");
+    carBrandCard.classList.add("carBrandCard");
+
+    const carBrand = document.createElement("h3");
+    carBrand.classList.add("carBrand");
+
+    const text = document.createTextNode(this.props.brand);
+    carBrand.append(text);
+
+    const carModels = document.createElement("ul");
+    for (let el of this.props.models) {
+      const li = document.createElement("li");
+      li.append(document.createTextNode(`${el}`));
+      carModels.append(li);
+    }
+
+    output.append(carBrandCard);
+    carBrandCard.append(carBrand, carModels);
+
+  }
+}
+
+function loadCarCards(fileLink) {
+
+  fetch(fileLink)
+    .then(response => response.json())
+    .then((data) => {
+      data.forEach(el => {
+        const newCarCard = new CarBrand(el);
+      });
+    });
+}
+
+loadCarCards(ENDPOINT);
