e1dc1c344fb0f291b4fe960628635918553c5c3b
giedrius-tumosa <gie.tumosa@gmail.com>
2023-01-02 13:14:25 +0200

    Completed Task 8.

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,20 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  constructor() { //default konstruktorius
+  }
+  sum(firstNum, secondNum) {
+    return firstNum + secondNum;
+  }
+  subtraction(firstNum, secondNum) {
+    return firstNum - secondNum;
+  }
+  multiplication(firstNum, secondNum) {
+    return firstNum * secondNum;
+  }
+  division(firstNum, secondNum) {
+    return firstNum / secondNum;
+  }
+}
