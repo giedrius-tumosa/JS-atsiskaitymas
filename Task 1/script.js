e34156bedc41e7beeb1475875bde4a58b0bbcc8a
giedrius-tumosa <gie.tumosa@gmail.com>
2023-01-02 10:08:17 +0200

    Completed Task 1.

---
+++ Task 1/script.js
@@ -0,0 +1,41 @@
+/* ------------------------------ TASK 1 ----------------------------
+Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
+pamatyti jo pateikto svorio konvertavimą į:
+1. Svarus (lb) | Formulė: lb = kg * 2.2046
+2. Gramus (g) | Formulė: g = kg / 0.0010000
+3. Uncijos (oz) | Formulė: oz = kg * 35.274
+
+Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
+<div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
+------------------------------------------------------------------- */
+
+const form = document.querySelector("#form-weightCalc");
+const output = document.querySelector("#output");
+const outputParagraph = document.createElement("p");
+output.append(outputParagraph);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Text reset
+  outputParagraph.textContent = "";
+  const weight_Kg = e.target.elements.search.valueAsNumber;
+  if (weight_Kg || weight_Kg === 0) {
+    const [weight_Lb, weight_G, weight_Oz] = [
+      weight_Kg * 2.2046,
+      weight_Kg / 0.0010000,
+      weight_Kg * 35.274
+    ];
+    const text = document.createTextNode(`
+      Jūsų svoris:
+      1. Svarais: ${weight_Lb.toFixed(1)}lb.
+      2. Gramais: ${weight_G.toFixed(1)}g.
+      3. Uncijomis: ${weight_Oz.toFixed(1)}oz.
+    `);
+    outputParagraph.append(text);
+  } else {
+    const text = document.createTextNode(`Klaida: neatpažinti duomenys.
+    Susiekite su administratoriumi.`);
+    outputParagraph.append(text);
+  }
+})
+
