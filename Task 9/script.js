69d1faf34f798bb10967d34168e0da7b62ffc12e
giedrius-tumosa <gie.tumosa@gmail.com>
2023-01-02 13:23:30 +0200

    Completed Task 9.

--- Task 9/script.js
+++ Task 9/script.js
@@ -2,7 +2,20 @@
 Sukurkite klasę "Movie", kuri sukuria objektus su 3 savybėm ir 1 metodu:
 
 Savybės:
-title, director, budget
+#title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  #title;
+  #director;
+  #budget;
+  constructor(title, director, budget) {
+    [this.#title, this.#director, this.#budget] = [title, director, budget];
+  }
+  wasExpensive() {
+    return this.#budget > 100000000;
+  }
+}
+
